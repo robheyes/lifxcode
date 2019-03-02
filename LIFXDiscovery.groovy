e06864e2ffe4891d793a6853030e58c9960616e4
Rob Heyes <rob@scl.co.uk>
2019-03-02 16:49:49 +0000

    Discovery now shows a progress percentage.
    No longer uses pauseExecution, now uses a queue and runInMillis
    
    Some code cleanup

--- LIFXDiscovery.groovy
+++ LIFXDiscovery.groovy
@@ -30,17 +30,17 @@ metadata {
 }
 
 def updated() {
-    log.debug "LIFX updating"
+    log.info "LIFX updating"
 }
 
 def installed() {
-    log.debug "LIFX Discovery installed"
+    log.info "LIFX Discovery installed"
     initialize()
 }
 
 def initialize() {
     def discoveryType = parent.discoveryType()
-    logDebug "Dscovery type $discoveryType"
+    logDebug "Discovery type $discoveryType"
     if (discoveryType == 'discovery') {
         refresh()
     } else if (discoveryType == 'refresh') {
@@ -53,65 +53,64 @@ def initialize() {
 def refresh() {
     String subnet = parent.getSubnet()
     if (!subnet) {
+        log.warn "Can't discover the hub's subnet!"
         return
     }
 
     parent.clearCachedDescriptors()
     def scanPasses = parent.maxScanPasses()
-
-    1.upto(scanPasses) {
-        logDebug "Scanning pass $it of $scanPasses"
-        parent.setScanPass(it)
-        scanNetwork subnet, it
-        logDebug "Pass $it complete"
-    }
-    parent.setScanPass 'DONE'
-    sendEvent name: 'lifxdiscovery', value: 'complete'
-}
-
-List<Map> discoverMacs(String subnet) {
-    def result = []
-    1.upto(254) {
-        def ipAddress = subnet + it
-        def mac = getMACFromIP(ipAddress)
-        if (mac) {
-            result << [mac: mac, ip: ipAddress]
-        }
-    }
-    result
-}
-
-private scanNetwork(String subnet, int pass) {
     String packet = makePacketString messageTypes().DEVICE.GET_VERSION.type as int, true, 1 as byte
-    1.upto(254) {
-        def ipAddress = subnet + it
-        if (!parent.isKnownIp(ipAddress)) {
-//            def mac = getMACFromIP(ipAddress)
-//            logDebug "Mac for $ipAddress = $mac"
-            1.upto(pass + extraProbesPerPass) {
-//            1.upto(2) {
-                sendPacket ipAddress, packet
-                pauseExecution(pass > 1 ? parent.interCommandPauseMilliseconds(pass) : 30)
-//                sendCommand ipAddress, messageTypes().DEVICE.GET_VERSION.type as int, true, 1, it % 128 as Byte
-            }
+    Map queue = prepareQueue(packet)
+    1.upto(scanPasses) {
+        parent.setScanPass(it)
+        scanNetwork queue, subnet, it
+    }
+    sendEvent name: 'progress', value: 0
+    queue.size = queue.ipAddresses.size()
+    runInMillis(50, 'processQueue', [data: queue])
+}
+
+private void scanNetwork(Map queue, String subnet, int pass) {
+    1.upto(pass + extraProbesPerPass) {
+        1.upto(254) {
+            def ipAddress = subnet + it
+            queue.ipAddresses << ipAddress
         }
-//        sendEvent name: 'progress', value: ((100 * it) / 255).toString()
     }
 }
 
-private scanNetwork(List<Map> macs, int pass) {
+private static Map prepareQueue(String packet) {
+    [packet: packet, ipAddresses: [], delay: 50]
+}
 
-    macs.each {
-        String ipAddress = it.ip
-        if (!parent.isKnownIp(ipAddress)) {
-            logDebug "Scanning $ipAddress"
-            1.upto(pass + extraProbesPerPass) {
-//            1.upto(2) {
-                sendCommand ipAddress, messageTypes().DEVICE.GET_VERSION.type as int, true, 1, it % 128 as Byte
-            }
-        }
-//        sendEvent name: 'progress', value: ((100 * it) / 255).toString()
+/** DND used by runInMillis */
+private processQueue(Map queue) {
+    def oldPercent = calculateQueuePercentage(queue)
+    if (isQueueEmpty(queue)) {
+        sendEvent name: 'lifxdiscovery', value: 'complete'
+        return
     }
+    def data = getNext(queue)
+    sendPacket data.ipAddress, data.packet
+    def newPercent = calculateQueuePercentage(queue)
+    if (oldPercent != newPercent) {
+        sendEvent name: 'progress', value: newPercent
+    }
+    runInMillis(queue.delay, 'processQueue', [data: queue])
+}
+
+private static Map<String, String> getNext(Map queue) {
+    String first = queue.ipAddresses.first()
+    queue.ipAddresses = queue.ipAddresses.tail()
+    [ipAddress: first, packet: queue.packet]
+}
+
+private static isQueueEmpty(Map queue) {
+    queue.ipAddresses.isEmpty()
+}
+
+private static int calculateQueuePercentage(Map queue) {
+    100 - (int) ((queue.ipAddresses.size() * 100) / queue.size as Long)
 }
 
 def rescanNetwork() {
@@ -148,7 +147,6 @@ private String makePacketString(int messageType, boolean responseRequired, byte 
 def parse(String description) {
     Map<String, List> command = parent.discoveryParse(description)
     sendActions command
-//    command != null ? sendCommand(command.ip as String, command.type as int) : null
 }
 
 private void sendActions(Map<String, List> actions) {
@@ -156,40 +154,11 @@ private void sendActions(Map<String, List> actions) {
     actions.events?.each { sendEvent it }
 }
 
-private void sendCommand(String ip, String deviceAndType, Map payload = [:], boolean responseRequired = true, boolean ackRequired = false, Byte index = 0) {
-//    resendUnacknowledgedCommand()
-    def parts = deviceAndType.split(/\./)
-    def buffer = []
-    byte sequence = parent.makePacket buffer, parts[0], parts[1], payload, responseRequired, ackRequired, index
-//    if (ackRequired) {
-//        parent.expectAckFor device, sequence, buffer
-//    }
-    sendPacket buffer
-}
-
-private void sendPacket(List buffer) {
-    String stringBytes = hubitat.helper.HexUtils.byteArrayToHexString parent.asByteArray(buffer)
-    sendHubCommand(
-            new hubitat.device.HubAction(
-                    stringBytes,
-                    hubitat.device.Protocol.LAN,
-                    [
-                            type              : hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT,
-                            destinationAddress: myIp() + ":56700",
-                            encoding          : hubitat.device.HubAction.Encoding.HEX_STRING
-                    ]
-            )
-    )
-}
-
 Map<String, Map<String, Map>> messageTypes() {
     parent.messageTypes()
 }
 
-private void sendPacket(String ipAddress, String bytes, boolean wantLog = false) {
-    if (wantLog) {
-        logDebug "sending bytes: ${stringBytes} to ${ipAddress}"
-    }
+private void sendPacket(String ipAddress, String bytes) {
     broadcast bytes, ipAddress
 }
 
@@ -225,4 +194,31 @@ private void logInfo(msg) {
 
 private void logWarn(String msg) {
     log.warn msg
+}
+
+
+/** ========= BELOW THIS LINE IS EXPERIMENTAL CODE THAT MAY PROVE USEFUL EVENTUALLY */
+List<Map> discoverMacs(String subnet) {
+    def result = []
+    1.upto(254) {
+        def ipAddress = subnet + it
+        def mac = getMACFromIP(ipAddress)
+        if (mac) {
+            result << [mac: mac, ip: ipAddress]
+        }
+    }
+    result
+}
+
+private scanNetwork(List<Map> macs, int pass) {
+
+    macs.each {
+        String ipAddress = it.ip
+        if (!parent.isKnownIp(ipAddress)) {
+            logDebug "Scanning $ipAddress"
+            1.upto(pass + extraProbesPerPass) {
+                sendCommand ipAddress, messageTypes().DEVICE.GET_VERSION.type as int, true, 1, it % 128 as Byte
+            }
+        }
+    }
 }
