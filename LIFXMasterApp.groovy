d9ce46b5a60a16a008b0516e3e09cdbdb6a79650
dkilgore90 <51180846+dkilgore90@users.noreply.github.com>
2020-07-04 13:39:09 -0400

    corrections post-merge

--- LIFXMasterApp.groovy
+++ LIFXMasterApp.groovy
@@ -679,7 +679,7 @@ Map<String, List> deviceOnOff(String value, Boolean displayed, duration = 0) {
     actions
 }
 
-Map<String, List> deviceSetZones(com.hubitat.app.DeviceWrapper device, Map zoneMap, Boolean extMZ, Boolean displayed = true) {
+Map<String, List> deviceSetZones(com.hubitat.app.DeviceWrapper device, Map zoneMap, Boolean extMZ, Boolean displayed = true, String power = 'on') {
     def actions = makeActions()
     if (extMZ) {
         actions.commands << makeCommand('MULTIZONE.SET_EXTENDED_COLOR_ZONES', zoneMap)
@@ -694,7 +694,7 @@ Map<String, List> deviceSetZones(com.hubitat.app.DeviceWrapper device, Map zoneM
 
     if (null != power && device.currentSwitch != power) {
         def powerLevel = 'on' == power ? 65535 : 0
-        actions.commands << makeCommand('LIGHT.SET_POWER', [powerLevel: powerLevel, duration: duration * 1000])
+        actions.commands << makeCommand('LIGHT.SET_POWER', [powerLevel: powerLevel, duration: zoneMap.duration * 1000])
         actions.events << [name: "switch", value: power, displayed: displayed, data: [syncing: "false"]]
     }
     actions
